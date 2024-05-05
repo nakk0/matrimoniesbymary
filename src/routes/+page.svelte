b4e6a544d072718ac265d79bc398bf8fe8bc068c
nakko <langonebros2@gmail.com>
2024-05-05 17:01:42 +0200

    added logo for a test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import logo from "$lib/images/logo.png";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button class="btn">test</button>
+<img src={logo}>
+

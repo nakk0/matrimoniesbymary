12c1afd8f0c7e724a4bf7c04dfabfd08a8b8cf24
nakko <langonebros2@gmail.com>
2024-05-23 17:39:20 +0200

    added routes and images. Finished navbar.

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,68 +1,87 @@
 <script>
   import logo from "$lib/images/logo.png";
+  import { page } from "$app/stores";
+
+  const links = [
+    {href: "/about-me", name: "about me"},
+    {href: "/ceremony-planning", name: "ceremony planning"},
+    {href: "/music-services", name: "music services"},
+    {href: "/gallery", name: "gallery"},
+    {href: "/vow-renewal", name: "vow renewal"},
+    {href: "/contact-me", name: "contact me"},
+  ]
+
 </script>
 
-<div class="navbar bg-base-100">
-  <div class="navbar-start">
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h8m-8 6h16"
-          /></svg
+<div class="bg-base-100">
+  <a href="/"><p class="text-6xl hidden lg:flex justify-center handycheera pt-3">Matrimonies by Mary <img src={logo} alt="Matrimonies by Mary" class="max-w-16 ms-5" /></p></a>
+  <div class="navbar">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            ><path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            /></svg
+          >
+        </div>
+        <ul
+          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
+          {#each links as link}
+            <li><a href="{link.href}" class:active={$page.url.pathname === link.href}>{link.name}</a></li>
+          {/each}
+          <!-- <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li> -->
+        </ul>
       </div>
-      <ul
-        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      <a class="btn btn-ghost text-xl flex lg:hidden" href="/"
+        ><img src={logo} alt="Matrimonies by Mary" class="max-w-11" />
+        <p class="lg:hidden handycheera">Matrimonies by Mary</p></a
       >
-        <li><a href="/about-me">about me</a></li>
-        <li><a href="/ceremony-planning">ceremony planning</a></li>
-        <li><a href="/music-services">music services</a></li>
-        <li><a href="/gallery">gallery</a></li>
-        <li><a href="/vow-renewal">vow renewal</a></li>
-        <li><a href="/contact-me">contact me</a></li>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        {#each links as link}
+            <li><a href="{link.href}" class:active={$page.url.pathname === link.href}>{link.name}</a></li>
+        {/each}
         <!-- <li>
-          <a>Parent</a>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <details>
+            <summary>Parent</summary>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
         </li> -->
       </ul>
     </div>
-    <a class="btn btn-ghost text-xl" href="/"
-      ><img src={logo} alt="Matrimonies by Mary" class="max-w-11" />
-      <p class="md:hidden">Matrimonies by Mary</p></a
-    >
-  </div>
-  <div class="navbar-center hidden lg:flex">
-    <ul class="menu menu-horizontal px-1">
-      <li><a href="/about-me">about me</a></li>
-      <li><a href="/ceremony-planning">ceremony planning</a></li>
-      <li><a href="/music-services">music services</a></li>
-      <li><a href="/gallery">gallery</a></li>
-      <li><a href="/vow-renewal">vow renewal</a></li>
-      <li><a href="/contact-me">contact me</a></li>
-      <!-- <li>
-        <details>
-          <summary>Parent</summary>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li> -->
-    </ul>
-  </div>
-  <div class="navbar-end">
-    <!-- <a class="btn">Button</a> -->
+    <div class="navbar-end">
+      <!-- <a class="btn">Button</a> -->
+    </div>
   </div>
 </div>
+
+<style>
+   @font-face {
+    font-family: 'handycheera';
+    src: url('$lib/fonts/Handycheera.otf') format('truetype');
+  }
+
+  .handycheera {
+    font-family: 'handycheera';
+  }
+</style>

0e7f9c11aab68e10068179347f7308b1c559646f
VincenzoLangone <vincenzo.langone@ontariotechu.net>
2025-01-22 18:20:07 -0500

    added new pictures

--- src/lib/images/gallery/index.js
+++ src/lib/images/gallery/index.js
@@ -1,21 +1,29 @@
-import IMG_6628 from './IMG_6628.jpeg';
-import IMG_7044 from './IMG_7044.jpeg';
-import IMG_7123 from './IMG_7123.jpeg';
-import unnamed1 from './unnamed1.jpg';
-import unnamed2 from './unnamed2.jpg';
-import IMG_7057 from './IMG_7057.jpeg';
+import imagenew2 from './imagenew2.png';
+import imagenew1 from './imagenew1.png';
 import unnamed3 from './unnamed3.jpg';
-import IMG_7906 from './IMG_7906.jpeg';
+import IMG_7057 from './IMG_7057.jpeg';
 import A from './A.jpeg';
+import imagenew4 from './imagenew4.png';
+import imagenew3 from './imagenew3.png';
+import IMG_7123 from './IMG_7123.jpeg';
+import unnamed2 from './unnamed2.jpg';
+import unnamed1 from './unnamed1.jpg';
+import IMG_7044 from './IMG_7044.jpeg';
+import IMG_7906 from './IMG_7906.jpeg';
+import IMG_6628 from './IMG_6628.jpeg';
 
 export default [
-  { name: 'IMG_6628', path: IMG_6628 },
-  { name: 'IMG_7044', path: IMG_7044 },
-  { name: 'IMG_7123', path: IMG_7123 },
-  { name: 'unnamed1', path: unnamed1 },
-  { name: 'unnamed2', path: unnamed2 },
-  { name: 'IMG_7057', path: IMG_7057 },
+  { name: 'imagenew2', path: imagenew2 },
+  { name: 'imagenew1', path: imagenew1 },
   { name: 'unnamed3', path: unnamed3 },
+  { name: 'IMG_7057', path: IMG_7057 },
+  { name: 'A', path: A },
+  { name: 'imagenew4', path: imagenew4 },
+  { name: 'imagenew3', path: imagenew3 },
+  { name: 'IMG_7123', path: IMG_7123 },
+  { name: 'unnamed2', path: unnamed2 },
+  { name: 'unnamed1', path: unnamed1 },
+  { name: 'IMG_7044', path: IMG_7044 },
   { name: 'IMG_7906', path: IMG_7906 },
-  { name: 'A', path: A }
+  { name: 'IMG_6628', path: IMG_6628 }
 ];

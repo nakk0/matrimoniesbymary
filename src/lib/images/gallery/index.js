883d52233e463019b3c722cc4efef431416227af
VincenzoLangone <vincenzo.langone@ontariotechu.net>
2026-07-07 20:25:33 +0200

    update gallery

--- src/lib/images/gallery/index.js
+++ src/lib/images/gallery/index.js
@@ -1,29 +1,41 @@
-import imagenew2 from './imagenew2.png';
-import imagenew1 from './imagenew1.png';
-import unnamed3 from './unnamed3.jpg';
-import IMG_7057 from './IMG_7057.jpeg';
 import A from './A.jpeg';
-import imagenew4 from './imagenew4.png';
-import imagenew3 from './imagenew3.png';
-import IMG_7123 from './IMG_7123.jpeg';
-import unnamed2 from './unnamed2.jpg';
-import unnamed1 from './unnamed1.jpg';
-import IMG_7044 from './IMG_7044.jpeg';
-import IMG_7906 from './IMG_7906.jpeg';
 import IMG_6628 from './IMG_6628.jpeg';
+import IMG_7044 from './IMG_7044.jpeg';
+import IMG_7057 from './IMG_7057.jpeg';
+import IMG_7123 from './IMG_7123.jpeg';
+import IMG_7906 from './IMG_7906.jpeg';
+import imagenew1 from './imagenew1.png';
+import imagenew2 from './imagenew2.png';
+import imagenew3 from './imagenew3.png';
+import imagenew4 from './imagenew4.png';
+import unnamed1 from './unnamed1.jpg';
+import unnamed2 from './unnamed2.jpg';
+import unnamed3 from './unnamed3.jpg';
+import IMG_3410 from './IMG_3410.jpeg';
+import IMG_3306 from './IMG_3306.jpeg';
+import IMG_2962 from './IMG_2962.jpeg';
+import jj from './jj.jpeg';
+import rd from './rd.jpeg';
+import c6 from './c6.jpeg';
 
 export default [
   { name: 'imagenew2', path: imagenew2 },
-  { name: 'imagenew1', path: imagenew1 },
-  { name: 'unnamed3', path: unnamed3 },
-  { name: 'IMG_7057', path: IMG_7057 },
-  { name: 'A', path: A },
-  { name: 'imagenew4', path: imagenew4 },
   { name: 'imagenew3', path: imagenew3 },
-  { name: 'IMG_7123', path: IMG_7123 },
-  { name: 'unnamed2', path: unnamed2 },
+  { name: 'imagenew4', path: imagenew4 },
   { name: 'unnamed1', path: unnamed1 },
+  { name: 'unnamed2', path: unnamed2 },
+  { name: 'unnamed3', path: unnamed3 },
+  { name: 'IMG_3410', path: IMG_3410 },
+  { name: 'IMG_3306', path: IMG_3306 },
+  { name: 'IMG_2962', path: IMG_2962 },
+  { name: 'jj', path: jj },
+  { name: 'rd', path: rd },
+  { name: 'c6', path: c6 },
+  { name: 'A', path: A },
+  { name: 'IMG_6628', path: IMG_6628 },
   { name: 'IMG_7044', path: IMG_7044 },
+  { name: 'IMG_7057', path: IMG_7057 },
+  { name: 'IMG_7123', path: IMG_7123 },
   { name: 'IMG_7906', path: IMG_7906 },
-  { name: 'IMG_6628', path: IMG_6628 }
+  { name: 'imagenew1', path: imagenew1 },
 ];

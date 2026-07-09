b34b8e8ce9aafce4600b8d713d86398c22b42eeb
VincenzoLangone <vincenzo.langone@ontariotechu.net>
2026-07-09 18:59:59 +0200

    added copy of navbar buttons to bottom of main page when small device (burger menu)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,43 @@
 
 <div>
     <div>
-        <h1 class="text-4xl md:text-6xl elegant-font text-yellow-500 text-center">Welcome to Matrimonies By Mary</h1>
-        <h2 class="mt-4 text-lg md:text-2xl elegant-font text-center">Creating memorable ceremonies through words and music</h2>
+        <div>
+            <h1 class="text-4xl md:text-6xl elegant-font text-yellow-500 text-center">Welcome to Matrimonies By Mary</h1>
+            <h2 class="mt-4 text-lg md:text-2xl elegant-font text-center">Creating memorable ceremonies through words and music</h2>
+        </div>
+        <img src="{mainpic}" alt="Main Image. Mary holding a microphone and a vow book" class="min-w-full shadow-xl rounded-xl">
     </div>
-    <img src="{mainpic}" alt="Main Image. Mary holding a microphone and a vow book" class="min-w-full shadow-xl rounded-xl">
-</div>
 
+    <div class="md:hidden text-center mt-3">
+        <a href="/about">
+            <button class="btn mt-2">
+            about me
+            </button>
+        </a>
+        <a href="/ceremonyPlanning">
+            <button class="btn mt-2">
+            ceremony planning
+            </button>
+        </a>
+        <a href="/musicServices">
+            <button class="btn mt-2">
+            music services
+            </button>
+        </a>
+        <a href="/vowRenewal">
+            <button class="btn mt-2">
+            vow renewal
+            </button>
+        </a>
+        <a href="/gallery">
+            <button class="btn mt-2">
+            gallery
+            </button>
+        </a>
+        <a href="/contact">
+            <button class="btn mt-2">
+            contact me
+            </button>
+        </a>
+    </div>
+</div>
